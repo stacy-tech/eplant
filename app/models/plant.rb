4dddd9c7403f9a771f4f8f15b566327a4a9683c8
stacy <stacydn23@gmail.com>
2021-02-08 21:58:55 -0500

    add validations in both the user and plant class

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,5 @@
 class Plant < ActiveRecord::Base
     has_many :user_plants
     has_many :users, through: :user_plants
-
+    validates :name, :type, :bloomscape, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :user_plants
     has_many :plants, through: :user_plants
+    validates :username, :email, :password_digest, presence: true
 end
