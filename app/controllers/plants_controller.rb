ed29bbd9297efc2289d334f3be70b7dc9b61620e
stacy <stacydn23@gmail.com>
2021-02-13 02:46:56 -0500

    update plants_controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,12 +2,7 @@ class PlantsController < ApplicationController
 
   get '/plants' do
     redirect_if_not_logged_in
-    binding.pry
     erb :'/plants/index.html'
-  end
-
-  post '/plants' do
-   
   end
 end
   
