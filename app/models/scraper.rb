1c25c24d439e096b7eec76ff7bedbfb27dce4ef1
stacy <stacydn23@gmail.com>
2021-02-19 02:56:37 -0500

    add plant scraper

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -3,23 +3,31 @@ require 'open-uri'
 require_relative "../../config/environment"
 
 class Scraper
-    def scrape_plants_urls
-        plant_url = 'https://www.houseplant411.com/houseplant'
-        html = open(plant_url)
-        doc = Nokogiri::HTML(html)
-        house_plants = doc.css('#resultsMid-outer').css('.resultsInd').css('a')
-        house_plants= []
-    end
-
-    def get_plants
-        house_plants.each do |plant|
+    
+    def scrape_page
+        url = "https://www.ourhouseplants.com/plants-hub"
+        html = open(url)
+        doc = Nokogiri::HTML(html) 
+        plant_tiles = doc.css("ul li a img")
+        plant_tiles.each do |plant|
+            
             binding.pry
+            hash = {
+                name: plant.attr("title")
+            }
+            Plant.new(hash)
         end
     end
+
+    
+
 end
 
-scrape = Scraper.new
- scrape.scrape_plants_urls
+Scraper.new.scrape_page
+
+
+
+
 
   
 
